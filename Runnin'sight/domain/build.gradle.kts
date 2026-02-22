c0910957b4b5937ad2eac096bbe0a15ef1e2f084
Ayewww <gudwns6518@gmail.com>
2026-02-22 12:43:09 +0900

    chore(domain,data): migrate build.gradle to Kotlin DSL

--- Runnin'sight/domain/build.gradle.kts
+++ Runnin'sight/domain/build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
-    id("java-library")
     alias(libs.plugins.jetbrains.kotlin.jvm)
 }
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+    jvmToolchain(21) // compilerOptions 대신 이 한 줄이면 충분
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
 }
